1bf2dec40c2b84ab173b33635245bd6be963475f
qixiaoling <x.qi@novi-education.nl>
2021-08-16 09:47:18 +0200

    sixth commit

---
+++ src/Component/PonyCard.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function PonyCard(){
+    return(<h1>pony card</h1>)
+}
+export default PonyCard;
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import HeroSection from "../Component/HeroSection";
 import CardSection from "../Component/CardSection";
+import axios from "axios";
 
 function Home() {
 
+    const[Rarity, setRarity] = useState(null);
+    const[twilight, setTwilight] = useState(null);
+    const[fluttershy, setFluttershy] = useState(null);
+    const[rainbow , setRainbow] = useState(null);
+    const[applejack, setApplejack] = useState(null);
+    const[pinkie , setPinkie] = useState(null);
 
+    async function getDataRarity () {
+        try{
+            const result = await axios
+        }catch(e){
+            console.error(e);
+        }
+    }
 
     return(
         <div className='home-container'>
